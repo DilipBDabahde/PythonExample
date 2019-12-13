70baf9437586eb243d6d6de819f174498b028f7d
DilipBDabahde <47924599+DilipBDabahde@users.noreply.github.com>
2019-12-13 13:52:16 +0530

    Add files via upload

---
+++ SnapshotOfAllRunningProcessOnRAM.py
@@ -0,0 +1,19 @@
+'''
+accept directory name from user and create Logfile.txt into that directory of all running processes on RAM
+
+'''
+
+
+
+
+def main():
+
+
+	print("Application Name: ",sys.argv[0]);
+
+
+
+
+
+if __name__ == "__main__":
+	main();
