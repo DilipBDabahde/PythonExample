5a1497316a14df77285b71f00e310b02a263e53f
DilipBDabahde <47924599+DilipBDabahde@users.noreply.github.com>
2019-12-27 01:38:14 +0530

    Add files via upload

--- ML/UserDefinedKNN.py
+++ ML/UserDefinedKNN.py
@@ -1,104 +1,75 @@
+
 """
-user defined KNN ALGO
-
-classifier: User Defined K Nearest Neighbour
-Dataset : 	Iris DataSet
-Features 	Sepel with,Sepal length, petal width petal length
-
-Labels:		Versicolr, setosa, Verginica
-Training Dataset:	75Entries
-Testing Dataset: 	75Entries
+user defined knn
 """
 
-from sklearn import tree
-from scipy.spatial import distance
 from sklearn.datasets import load_iris
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+import sys
+from scipy.spatial import distance
+from sklearn.metrics import accuracy_score
+
+def euc(x, y):
+	return distance.euclidean(x, y);
 
 
-def euc(a,b):
+class UserDefinedKNN:
 
-	return distance.euclidean(a,b);
+	def fit(self,data_x, target_x):
+		self.data_x = data_x;
+		self.target_x = target_x;
+	
+	def predict(self, data_y):
+		prediction = [];
 
-
-class MarvellousKNN():
-	def fit(self, TrainingData, TrainingTarget):
-		self.TrainingData = TrainingData
-		self.TrainingTarget = TrainingTarget
-		
-	def predict(self, TestData):
-		predictions = []
-		for row in TestData:
+		for row in data_y:
 			label = self.closest(row);
-			predictions.append(label);
-		
-		return predictions
+			prediction.append(label);
+		return prediction;
 
 	def closest(self, row):
-		bestDistance = euc(row,self.TrainingData[0]);
-		print(bestDistance);
-		
-		for i in range(1,len(self.TrainingData)):
-			dist= euc(row, self.TrainingData[i]);
-			if dist < bestDistance:
-				bestDistance = dist;
-				bestIndex = i;
-		return self.TrainingTarget[bestIndex];
-			
-	
-def MarvellousKNeighbor():
-	border = "-"*77;
-	
-	iris = load_iris(); # data loaded
-	
-	data = iris.data;
-	target = iris.target;
-	
-	print(border);
-	print("Actual data set");
-	print(border);
-	
-	for i in range(len(iris.target)):
-		print("ID: %d, Feature %s Label %s"%(i,iris.data[i],iris.target[i]))
-	
-	print("Size of actual data set %d"%(i+1));
-	
-	data_train, data_test, target_train, target_test = train_test_split(data,target,test_size=0.5); # here we divide all data into 50 50 format 
-	
-	
-	print(border);
-	print("Training data set")
-	print(border);
-	
-	for i in range(len(data_train)):
-		print("ID: %d, Label %s, Features: %s"%	(i,data_train[i],target_train[i]));
-	
-	print("size of traing data set:%d"%(i+1))
-	
-	
-	print(border);
-	print("Test data set");
-	print(border);
-	
-	for i in range(len(data_test)):
-		print("ID: %d, Label %s, Features: %s"%(i,data_test[i],target_test[i]));
-		
-	print("size of test data set %d"%(i+1));
-	print(border);
-	
-	
-	classifier = MarvellousKNN(); # mknn obj creataed
-	classifier.fit(data_train,target_train); # call to fit with dataset
-	predictions = classifier.predict(data_test); # prediction our datatest 
-	Accuracy = accuracy_score(target_test, predictions);
+		basedistance = euc(row, self.data_x[0]);
+		best_index = 0;
+		for i in range(1, len(self.data_x)):
+			dist = euc(row, self.data_x[i]);
+			if dist < basedistance:
+				basedistance = dist;
+				best_index = i;
+
+		return self.target_x[best_index];
+
+
+def RenewKNN():
+
+	iris = load_iris(); #step1 gathering data
+
+	data = iris.data;	# step2 cleaning dataset
+	target =iris.target;
+	data_x, data_y, target_x, target_y = train_test_split(data, target, test_size=0.5);
+
+
+	clf = UserDefinedKNN();	# step3 fix algo
+
+	clf.fit(data_x, target_x);	# step4 training 
+
+	predict = clf.predict(data_y);
+	print(predict);
+
+	Accuracy = accuracy_score(target_y, predict);
+
 	return Accuracy;
-	
 
 def main():
-	Accuracy = MarvellousKNeighbor();
-	print("Accuracy of classification algo with K neighbor classify is ",Accuracy*100,"%");	
-	
 
-if __name__ == "__main__":
+	print("Application Name: ",sys.argv[0]);
+
+	try:
+
+		Accuracy = RenewKNN();
+		print("Accuracy is :",Accuracy*100,"%");
+	except Exception as e:
+		print("Exception occured: ",e);
+
+
+if __name__ =="__main__":
 	main();
