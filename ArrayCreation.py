51f7e6cdf25565b3cc15a6d361be245fb2ddd5f4
DilipBDabahde <47924599+DilipBDabahde@users.noreply.github.com>
2020-02-08 12:52:23 +0530

    Add files via upload

---
+++ ArrayCreation.py
@@ -0,0 +1,13 @@
+# to create array in python we need to use library function names as array
+
+
+import array as arr
+
+# arr is object of array class
+
+our_arr = arr.array('i',[11,12,13,14,15]);
+
+# our_arr is array name 
+# arr.array(paremeters); here arr is object and array() is fun
+
+print(our_arr);
